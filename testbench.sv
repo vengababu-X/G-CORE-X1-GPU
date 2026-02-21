2261903154fa3fbba32837f29219b908d0c6ca0f
Boligorla vengababu <boligorlavengababu@gmail.com>
2026-02-21 22:00:30 +0530

    Refactor testbench for simd_gpu_core module

--- testbench.sv
+++ testbench.sv
@@ -1,13 +1,27 @@
 `timescale 1ns/1ps
+
 module tb_simd_gpu_core;
-reg clk, reset;
-simd_gpu_core dut(.clk(clk), .reset(reset));
+
+reg clk;
+reg reset;
+
+simd_gpu_core dut (
+    .clk(clk),
+    .reset(reset)
+);
+
 always #5 clk = ~clk;
+
 initial begin
     $dumpfile("layer4_memory.vcd");
     $dumpvars(0, tb_simd_gpu_core);
-    clk=0; reset=1;
-    #10 reset=0;
-    #120 $finish;
+
+    clk = 0;
+    reset = 1;
+    #10 reset = 0;
+
+    #120;
+    $finish;
 end
+
 endmodule
